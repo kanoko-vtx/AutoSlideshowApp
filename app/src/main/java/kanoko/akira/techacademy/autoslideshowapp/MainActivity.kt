c9862244c32329622b8445491f14434f9253a5e5
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-10 19:01:35 +0900

    ボタンを作成するところまで

--- app/src/main/java/kanoko/akira/techacademy/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/kanoko/akira/techacademy/autoslideshowapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -46,22 +47,18 @@ class MainActivity : AppCompatActivity() {
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor = resolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
-            null, // 項目(null = 全項目)
-            null, // フィルタ条件(null = フィルタなし)
-            null, // フィルタ用パラメータ
-            null // ソート (null ソートなし)
-        )
+        val cursor :Cursor? = resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null)
 
-        if (cursor.moveToFirst()) {
-            // indexからIDを取得し、そのIDから画像のURIを取得する
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
-            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                // indexからIDを取得し、そのIDから画像のURIを取得する
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-            this.imageView.setImageURI(imageUri)
+                imageView.setImageURI(imageUri)
+            }
+            cursor.close()
         }
-        cursor.close()
     }
 }
