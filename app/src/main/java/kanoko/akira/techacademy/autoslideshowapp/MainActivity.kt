713204e65910f2b7b2f413b7b00cf564ff0f04e0
kanoko-vtx <a,kanoko@vortex-gi.com>
2019-04-11 17:10:44 +0900

    画像一覧を配列に入れて表示するところまで

--- app/src/main/java/kanoko/akira/techacademy/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/kanoko/akira/techacademy/autoslideshowapp/MainActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
 import android.database.Cursor
+import android.net.Uri
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -47,18 +48,32 @@ class MainActivity : AppCompatActivity() {
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor :Cursor? = resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null)
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
+            null, // 項目(null = 全項目)
+            null, // フィルタ条件(null = フィルタなし)
+            null, // フィルタ用パラメータ
+            null // ソート (null ソートなし)
+        )
 
-        if (cursor != null) {
-            if (cursor.moveToFirst()) {
+        if (cursor.moveToFirst()) {
+
+            val imaglist = mutableListOf<String>()
+
+            do {
                 // indexからIDを取得し、そのIDから画像のURIを取得する
                 val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
                 val id = cursor.getLong(fieldIndex)
                 val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-                imageView.setImageURI(imageUri)
-            }
+                imaglist.add(imageUri.toString())
+
+                Log.d("list", "$imaglist")
+//            this.imageView.setImageURI(imageUri)
+            } while (cursor.moveToNext())
+            this.imageView.setImageURI(Uri.parse(imaglist[1]))
             cursor.close()
         }
     }
+
 }
